a924d7228feb988d24abfae0e9b084af7e0bbd5a
Julian Gründner <juliangruendner@googlemail.com>
2022-04-06 13:15:56 +0200

    add basic auth for fhir server and https proxy option for aktin broker connection

--- site-client/aktin-fhir-mii-report-client.py
+++ site-client/aktin-fhir-mii-report-client.py
@@ -2,16 +2,28 @@ import requests
 import json
 import argparse
 from datetime import datetime
+from requests.auth import HTTPBasicAuth
 
 parser = argparse.ArgumentParser()
 parser.add_argument('--fhirurl', help='base url of your local fhir server', default="http://localhost:8081/fhir")
+parser.add_argument('--fhiruser', help='basic auth user fhir server', nargs="?", default="")
+parser.add_argument('--fhirpw', help='basic auth pw fhir server', nargs="?", default="")
 parser.add_argument('--brokerurl', help='base url of the central aktin broker', default="http://localhost:8082/broker/")
 parser.add_argument('--apikey', help='your api key', default="xxxApiKey123")
+parser.add_argument('--httpsproxy', help='your https proxy url', nargs="?", default="")
 args = vars(parser.parse_args())
 
 fhir_base_url = args["fhirurl"]
+fhir_user = args["fhiruser"]
+fhir_pw = args["fhirpw"]
 aktin_broker_url = args["brokerurl"]
 aktin_broker_api_key = args["apikey"]
+aktin_broker_api_key = args["apikey"]
+https_proxy = args["httpsproxy"]
+
+proxies = {
+    "https": https_proxy
+}
 
 aktin_broker_node_url = f'{aktin_broker_url}my/node/miireport'
 
@@ -20,8 +32,10 @@ with open('report-queries.json') as json_file:
     report['datetime'] = datetime.now().strftime("%Y-%m-%d_%H-%M-%S")
     queries = report["queries"]
     for query in queries:
-        resp = requests.get(fhir_base_url + query["query"]).json()
+        resp = requests.get(f'{fhir_base_url}{query["query"]}', auth=HTTPBasicAuth(
+            fhir_user, fhir_pw), proxies=proxies).json()
         query['response'] = resp['total']
 
 headers = {'Authorization': f"Bearer {aktin_broker_api_key}"}
-resp = requests.put(aktin_broker_node_url, json=json.dumps(report), headers=headers)
+resp = requests.put(aktin_broker_node_url,
+                    json=json.dumps(report), headers=headers)
--- site-client/docker-entrypoint.sh
+++ site-client/docker-entrypoint.sh
@@ -4,4 +4,5 @@ FHIR_BASE_URL=${FHIR_BASE_URL:-"http://fhir-server:8080/fhir"}
 BROKER_ENDPOINT_URI=${BROKER_ENDPOINT_URI:-"http://aktin-broker:8080/broker/"}
 CLIENT_AUTH_PARAM=${CLIENT_AUTH_PARAM:-"xxxApiKey123"}
 
-python3 aktin-fhir-mii-report-client.py --fhirurl $FHIR_BASE_URL --brokerurl $BROKER_ENDPOINT_URI --apikey $CLIENT_AUTH_PARAM
+
+python3 aktin-fhir-mii-report-client.py --fhirurl $FHIR_BASE_URL --brokerurl $BROKER_ENDPOINT_URI --apikey $CLIENT_AUTH_PARAM --fhiruser $FHIR_USER --fhirpw $FHIR_PW --httpsproxy $HTTPS_PROXY
